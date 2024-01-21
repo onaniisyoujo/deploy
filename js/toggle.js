fe5ed9dfeda168934fcae79b28c20ed8b7ea510a
onaniisyoujo <onaniisyoujo@users.noreply.github.com>
2024-01-21 12:34:05 +0000

    Automated Deployment at 2024-01-21 12:34:04 2c0b894ece4347abe503e678a65b71c2b4bb44e4

--- js/toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleCSS() {
-      var linkElement = document.getElementById("custom-css");
-      if (linkElement.disabled) {
-        linkElement.disabled = false; // 启用CSS文件
-      } else {
-        linkElement.disabled = true; // 禁用CSS文件
-      }
-    }
